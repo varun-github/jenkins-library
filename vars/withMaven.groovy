a1b8c112734af54682f19e733b81aa4dc40350d3
Varun <write2varun@gmail.com>
2021-09-04 16:48:08 +0530

    test pipeline as arg

--- vars/withMaven.groovy
+++ vars/withMaven.groovy
@@ -1,7 +1,7 @@
 // import com.varun.jenkins.maven.MavenBuilder
 // import groovy.json.JsonSlurper
 
-def call(config = {}){
+def call(pipeline, config = {}){
     if (!config.withArgFile)  {
         print("mandatory argument withArgFile not supplied, doing nothing")
         return
@@ -10,8 +10,8 @@ def call(config = {}){
     // def jsonSlurper = new JsonSlurper()
 
     def mvnConfig = readJSON file: config.withArgFile
-    print("pipeline config is ${config.pipeline}")
-    print("env config is ${config.pipeline.env.HOME}")
+    print("pipeline config is ${pipeline}")
+    print("env config is ${pipeline.env.HOME}")
     // bat("${mvnConfig.MAVEN_HOME}/bin/mvn -s ${mvnConfig.MAVEN_SETTINGS} -DskipTests ${config.do}")
 
 }
