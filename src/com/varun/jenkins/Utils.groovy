360d05d932516b2f16e53304eff78d6353e7b46d
Varun <write2varun@gmail.com>
2021-09-04 19:43:41 +0530

    test library as a class

--- src/com/varun/jenkins/Utils.groovy
+++ src/com/varun/jenkins/Utils.groovy
@@ -14,13 +14,21 @@ class Utils implements Serializable{
         }
         // def txt = readFile(config.withArgFile)
         // def jsonSlurper = new JsonSlurper()
-
         def mvnConfig = pipeline.readJSON file: config.withArgFile
-        // print("pipeline config is ${this.pipeline}")
-        pipeline.print("mvn config is ${mvnConfig}")
-        this.pipeline.print("env config is ${pipeline.env.GIT_URL}")
+        this.pipeline.print("maven env config is ${mvnConfig}")
         pipeline.bat("${mvnConfig.MAVEN_HOME}/bin/mvn -s ${mvnConfig.MAVEN_SETTINGS} -DskipTests ${config.do}")
-
-
+    }
+    def withDocker(config){
+        if (!config.withArgFile)  {
+            print("mandatory argument withArgFile not supplied, doing nothing")
+            return
+        }
+        def dockerConfig = pipeline.readJSON file: config.withArgFile
+        this.pipeline.print("docker env config is ${dockerConfig}")
+        // TODO: handle the npe possiblity below
+        def git_repo = this.pipeline.env.GIT_URL.replaceFirst(/.*\/([\w-]+).*/, '$1')
+        this.pipeline.bat("aws --profile ${dockerConfig.AWS_PROFILE} --region ${dockerConfig.AWS_REGION} ecr get-login-password  | docker login --username AWS --password-stdin ${dockerConfig.ECR_REGISTRY}")
+        def img = this.pipeline.docker.build("${dockerConfig.ECR_REGISTRY}/${dockerConfig.DOCKER_REPO_PREFIX}/${git_repo}:${env.BRANCH_NAME}.${this.pipeline.env.BUILD_ID}")
+        img.push()
     }
 }
