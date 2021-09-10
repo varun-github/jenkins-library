3f9bbe4d663c014f0f607b0b1021c0d704e45549
Varun <write2varun@gmail.com>
2021-09-10 16:05:10 +0530

    test telemetry output

--- src/com/varun/jenkins/telemetry/JUnitEmitter.groovy
+++ src/com/varun/jenkins/telemetry/JUnitEmitter.groovy
@@ -2,6 +2,7 @@ package com.varun.jenkins.telemetry
 
 
 import hudson.tasks.test.AbstractTestResultAction
+import hudson.tasks.junit.TestResultSummary
 
 public class JUnitEmitter implements  Serializable{
     def pipeline
@@ -10,8 +11,12 @@ public class JUnitEmitter implements  Serializable{
         this.pipeline = pipeline
     }
 
-    def emitTestStatus(){
-        def hudson.tasks.test.AbstractTestResultAction action = this.pipeline.currentBuild.getRawBuild().getAction(AbstractTestResultAction.class)
-        return action.getResult().toString()
+    def recordTestsAndEmit(){
+        def TestResultSummary summary = this.pipeline.junit("target/surefire-reports/TEST*.xml")
+        this.pipeline.echo("test summary. passCount =" +summary.passCount+ ", failCount="+ summary.failCount+ ", totalCount=" +summary.totalCount+ ", skipCount=" +summary.skipCount)
+//        getResult below returns a hudson.tasks.junit.TestResult, not just a summary.
+
+//        def hudson.tasks.test.AbstractTestResultAction action = this.pipeline.currentBuild.getRawBuild().getAction(AbstractTestResultAction.class)
+//        def result = action.getResult()
     }
 }
